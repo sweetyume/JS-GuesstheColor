a3c0af6afd35d27a55834d96f35faaee9b28e930
sweetyume <sweet.tsukiyume@gmail.com>
2017-07-24 12:25:15 +0200

    finish

--- code.js
+++ code.js
@@ -2,9 +2,14 @@ var color =  ["yellow", "red", "blue"];
 
 function game () {
 
-  document.getElementById('color-box').style.backgroundColor = color[Math.floor(Math.random() * 3)];
+  document.getElementById('colorBox').style.backgroundColor = color[Math.floor(Math.random() * 3)];
+  if (document.getElementById('colorBox').style.backgroundColor == document.getElementById('colorSelect').value ) {
+    alert ("You win!");
+  }
 
-
+else {
+  alert ("You lose!");
+}
 }
 
 document.getElementById('bouton').addEventListener("click", game);
